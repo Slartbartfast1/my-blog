a93491698c0716240eac9e80d363950605c5779a
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-08-02 22:58:08 +0800

    验证登陆

--- updateUserInfo.php
+++ updateUserInfo.php
@@ -8,15 +8,11 @@ function edit()
     if (!$conn) {
         exit('<h1>连接失败</h1>');
     }
-    $query = mysqli_query($conn, 'select * from user');
-    if (!$query) {
-        exit('<h1>查询失败</h1>');
-    }
     $username = $_POST['name'];
     $email = $_POST['email'];
     $intro = $_POST['intro'];
     $avatarFile = $_FILES['avatar'];
-    $avatar = 'avatar/' . $avatarFile['name'];
+    $dest = 'avatar/' . $avatarFile['name'];
     //TODO:验证昵称 邮箱 个人简介 不能为空
     if (empty($username)) {
         exit('昵称不能为空');
@@ -27,12 +23,13 @@ function edit()
     if (empty($intro)) {
         exit('个人简介不能为空');
     }
-    move_uploaded_file($avatarFile['tmp_name'], $avatar);
+    move_uploaded_file($avatarFile['tmp_name'], $dest);
     //TODO: 如果上传头像为空,头像路径不变;
     if ($_FILES['avatar']["name"] == "") {
         mysqli_query($conn, "update user set name='$username',introduction='$intro',email='$email'");
     } else {
-        mysqli_query($conn, "update user set avatarurl='$avatar',name='$username',introduction='$intro',email='$email'");
+        mysqli_query($conn, "update user set avatarurl='$dest',name='$username',introduction='$intro',email='$email'");
     }
 }
+
 
