50ae3a4b404b364c4f3dcb75cd0858556134e957
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-08-17 14:36:40 +0800

    导航栏

--- admin/navBar.php
+++ admin/navBar.php
@@ -1,16 +1,15 @@
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <div class="container">
     <ul class="navbar-nav">
 
         <li class="nav-item ">
-            <a class= "nav-link <?php echo $_SERVER['PHP_SELF'] === '/Myblog/admin/adminIndex.php' ? "active" : '' ?>"
+            <a class= "nav-link <?php echo $_SERVER['PHP_SELF'] === '/admin/adminIndex.php' ? "active" : '' ?>"
                href="adminIndex.php">网站总览</a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown <?php echo $_SERVER['PHP_SELF'] == '/Myblog/admin/musicAdd.php' ? " active" : '' ?>
-<?php echo $_SERVER['PHP_SELF'] == '/Myblog/admin/musicEdit.php' ? " active" : '' ?>">
-            <a class="nav-link dropdown-toggle " href="#" id="navbardrop" data-toggle="dropdown">
+        <li class="nav-item dropdown <?php echo $_SERVER['PHP_SELF'] == '/admin/musicAdd.php' ? " active" : '' ?>
+<?php echo $_SERVER['PHP_SELF'] == '/admin/musicEdit.php' ? " active" : '' ?>">
+            <a class="nav-link dropdown-toggle <?php echo $_SERVER['PHP_SELF'] === '/admin/musicAdd.php' ? "active" : '' ?>
+<?php echo $_SERVER['PHP_SELF'] === 'admin/musicEdit.php' ? "active" : '' ?>" href="#" id="navbardrop" data-toggle="dropdown">
                 资源管理
             </a>
             <div class="dropdown-menu">
@@ -19,9 +18,9 @@
             </div>
         </li>
         <li class="nav-item dropdown
-<?php echo $_SERVER['PHP_SELF'] === '/Myblog/admin/articleAdd.php' ? "active" : '' ?>
-<?php echo $_SERVER['PHP_SELF'] === '/Myblog/admin/articles.php' ? "active" : '' ?>
-<?php echo $_SERVER['PHP_SELF'] === '/Myblog/admin/categories.php' ? "active" : '' ?>">
+<?php echo $_SERVER['PHP_SELF'] === '/admin/articleAdd.php' ? "active" : '' ?>
+<?php echo $_SERVER['PHP_SELF'] === '/admin/articles.php' ? "active" : '' ?>
+<?php echo $_SERVER['PHP_SELF'] === '/admin/categories.php' ? "active" : '' ?>">
             <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
                 文章管理
             </a>
@@ -38,14 +37,13 @@
             <div class="dropdown-menu">
 <!--                <a class="dropdown-item" href="#">导航菜单</a>-->
                 <a class="dropdown-item" href="sliderSeetings.php">图片轮播</a>
-                <a class="dropdown-item" href="#">网站设置</a>
             </div>
         </li>
 
-        <li class="nav-item <?php echo $_SERVER['PHP_SELF'] == '/Myblog/admin/commentEdit.php' ? "active" : '' ?>">
+        <li class="nav-item <?php echo $_SERVER['PHP_SELF'] == '/admin/commentEdit.php' ? "active" : '' ?>">
             <a class="nav-link " href="commentEdit.php" >评论管理</a>
         </li>
-        <li class="nav-item <?php echo $_SERVER['PHP_SELF'] === '/Myblog/admin/userInfoEdit.php' ? "active" : '' ?>">
+        <li class="nav-item <?php echo $_SERVER['PHP_SELF'] === '/admin/userInfoEdit.php' ? "active" : '' ?>">
             <a class="nav-link "  href="userInfoEdit.php">用户信息</a>
         </li>
         <li class="nav-item pull-right" >
@@ -53,7 +51,9 @@
             <a class="nav-link exit" href="login.php?action=logout" >退出</a>
         </li>
     </ul>
+</div>
 </nav>
+
 <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/4.1.1/css/bootstrap.css
 ">
 <script src="../static/assets/vendors/jQuery/jQuery.js"></script>
