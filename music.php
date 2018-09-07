eeafbcc336a2ea9b296d172798fd9c800cb348d3
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-09-07 10:37:04 +0800

    音乐盒

--- music.php
+++ music.php
@@ -1,15 +1,10 @@
-<?php include 'navBar.php';
+<?php
 require_once 'static/function.php';
 header("Content-Type: text/html;charset=utf-8");
 $section1=myFetchAll("select * from music where album=1");
 $section2=myFetchAll("select * from music where album=2");
 $section3=myFetchAll("select * from music where album=3");
 $section4=myFetchAll("select * from music where album=4");
-
-
-
-
-
 
  ?>
 <!DOCTYPE html>
@@ -18,8 +13,9 @@ $section4=myFetchAll("select * from music where album=4");
     <meta charset="UTF-8">
     <title>音乐盒</title>
 </head>
-<link href="static/assets/vendors/bootstrap/bootstrap.min.css" rel="stylesheet">
-<link href="static/assets/vendors/animate/animate.min.css" rel="stylesheet">
+<link rel="icon" href="/favicon.ico">
+<link href="https://cdn.bootcss.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.bootcss.com/animate.css/3.5.2/animate.min.css" rel="stylesheet">
 <link rel="stylesheet" href="static/assets/css/main.css">
 <link href="static/assets/vendors/aplayer/APlayer.min.css" rel="stylesheet">
 <link rel="stylesheet" href="static/assets/css/footer.css">
@@ -27,11 +23,10 @@ $section4=myFetchAll("select * from music where album=4");
 <link rel="stylesheet" href="static/assets/css/footer.css">
 </head>
 <body>
+<?php include 'navBar.php'; ?>
 <div class="page">
 
     <div class="block pt-5">
-        <!--        <h1 class="display-4 text-center">西部世界</h1>-->
-        <!--        <hr>-->
         <div class="container ">
             <div class="row">
                 <div class="col-lg-6 col-xs-12">
@@ -45,7 +40,6 @@ $section4=myFetchAll("select * from music where album=4");
                 <div class="firstBox  col-lg-6 col-xs-12">
                     <?php foreach ($section1 as $item): ?>
                     <div class="mt-2 musicBox">
-
                         <div class="postImg">
                             <div class="shadow">
                             </div>
@@ -58,7 +52,6 @@ $section4=myFetchAll("select * from music where album=4");
                         </div>
                     </div>
                     <?php endforeach; ?>
-
                 </div>
             </div>
         </div>
@@ -70,7 +63,8 @@ $section4=myFetchAll("select * from music where album=4");
             <div id="carousel" >
                 <div class="carouselWrap">
                     <?php foreach ($section2 as $item): ?>
-                    <img class="postImg" src="<?php echo $item['posterurl'] ?>" alt=""/> <span class="iconfont" style="display: none"></span>
+                    <img class="postImg" src="<?php echo $item['posterurl'] ?>" alt=""/>
+                        <span class="iconfont" style="display: none"></span>
                     <?php endforeach; ?>
             </div>
         </div>
@@ -82,9 +76,13 @@ $section4=myFetchAll("select * from music where album=4");
         <div class="container">
             <div class="row  d-flex justify-content-around my-3">
                 <?php foreach ($section3 as $item): ?>
-                <div class="col-lg-3 col-xs-8 musicSmall mx-2 my-4 wow animated fadeInDown"><div class="postImg postSmall"><div class="play text-center">
+                <div class="col-lg-3 col-xs-8 musicSmall mx-2 my-4 wow animated fadeInDown">
+                    <div class="postImg postSmall"><div class="play text-center">
                             <span class="iconfont"></span>
-                        </div><img src="<?php echo $item['posterurl'] ?>" alt=""></div><div class="musicSmallInfo"><p><?php echo $item['title'] ?></p><p>by <?php echo $item['author'] ?></p></div></div>
+                        </div><img src="<?php echo $item['posterurl'] ?>" alt=""></div>
+                    <div class="musicSmallInfo"><p><?php echo $item['title'] ?></p><p>by <?php echo $item['author'] ?></p>
+                    </div>
+                </div>
                 <?php endforeach; ?>
 
             </div>
@@ -119,19 +117,19 @@ $section4=myFetchAll("select * from music where album=4");
                 <div class="QR"><img src="static/assets/img/微信图片_20180815234204.jpg" alt="" class="img-fluid"></div>
                 <a href="#" class="icon1 wechat"><span></span></a>
                 <a href="https://blog.csdn.net/Slartibartfast" class="icon1 csdn"><span></span></a>
-                <a href="https://github.com/Slartbartfast1" class="icon1 github"><span></span></a>
+                <a href="https://github.com/Slartbartfast1/Myblog" class="icon1 github"><span></span></a>
 
             </div>
-
-            <small class="text-muted">15212068@bjtu.edu.cn</small>
-            <p class="text-muted">© 2018 泛银河系含漱爆破液</p>
+            <?php $user=myFetchOne("select * from user where userid='huangrui1019';"); ?>
+            <small class="text-muted"> <?php echo $user['email'] ?>  京ICP备18046047号</small>
+            <p class="text-muted">© 2018 <?php echo $user['name'] ?></p>
         </div>
     </div>
 
 </div>
 
 
-<!--================================================================================================================================-->
+
 
 <!--用PHP渲染播放器信息-->
 <div id="aplayer"></div>
@@ -169,17 +167,14 @@ $section4=myFetchAll("select * from music where album=4");
     </div>
 <?php endforeach; ?>
 
-<!--//php待渲染-->
 
-
-<script src="static/assets/vendors/jQuery/jQuery.js"></script>
-<script src="static/assets/vendors/bootstrap/bootstrap.bundle.min.js"></script>
-<script src="static/assets/vendors/wow/wow.min.js"></script>
+<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdn.bootcss.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="https://cdn.bootcss.com/wow/1.1.2/wow.min.js"></script>
 <script src="static/assets/js/navBar.js"></script>
 <script src="static/assets/vendors/waterwheelCarousel/jquery.waterwheelCarousel.min.js"></script>
 <script src="static/assets/vendors/aplayer/APlayer.min.js"></script>
 <script src="static/assets/vendors/waterwheelCarousel/jquery.waterwheelCarousel.min.js"></script>
 <script src="static/assets/js/music.js"></script>
-
 </body>
 </html>
