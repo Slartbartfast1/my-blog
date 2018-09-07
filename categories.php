67e0ae226642b20cc1de38041fd4647bd78ae7f0
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-09-07 10:37:42 +0800

    分类页

--- categories.php
+++ categories.php
@@ -1,5 +1,5 @@
 <?php
-include 'navBar.php';
+
 require_once 'static/function.php';
 header("Content-Type: text/html;charset=utf-8");
 $categories=myFetchAll('select * from categories');
@@ -14,93 +14,24 @@ $categories=myFetchAll('select * from categories');
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-<link href="static/assets/vendors/bootstrap/bootstrap.min.css" rel="stylesheet">    <link href="static/assets/vendors/animate/animate.min.css" rel="stylesheet">
+    <title>分类</title>
+<link href="https://cdn.bootcss.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.bootcss.com/animate.css/3.5.2/animate.min.css" rel="stylesheet">
     <link rel="stylesheet" href="static/assets/css/main.css">
     <link rel="stylesheet" href="static/assets/css/footer.css">
+    <link rel="stylesheet" href="static/assets/css/categories.css">
     <style>
         .page {
             z-index: 1;
             width: 100%;
-            background: url("static/assets/img/wallhaven-677930.jpg") no-repeat fixed;
+            background: url("https://upload-images.jianshu.io/upload_images/13265578-d80f90b3fce1e76e.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/4095/format/webp") no-repeat fixed;
             background-size: 100% 400px;
         }
-        .category{
-            position:relative;
-            height:300px;
-            width:300px;
-            background-color: pink;
-            top:-120px;
-            box-shadow: 0 0 1px 2px rgba(0,0,0,.1);
-            display: flex;
-            justify-content:center;
-            margin: 80px;
-
-        }
-        .category img{
-            height:100%;
-            width:100%;
-        }
-        .shadow{
-            height:100%;
-            width:100%;
-            background-color: rgba(0,0,0,.4);
-            z-index: 2;
-            position:absolute;
-            transition:.2s ease 0s;
-        }
-        .category:hover .shadow{
-            background-color: rgba(0,0,0,.6);
-        }
-        .categoryName{
-            position:absolute;
-            width:100%;
-            top:50%;
-            transform: translateY(-50%);
-            font-size: 40px;
-            font-family:"Microsoft JhengHei UI Light";
-            color:white;
-            z-index: 3;
-        }
-
-        .container1{
-            position: relative;
-            top:400px;
-            height:400px;
-            display: block;
-        }
-
-        @media screen and (min-width: 320px) and (max-width: 480px) {
-
-            .category{
-                height:100px;
-                width:100px;
-                float: left;
-                display: inline-block;
-                margin-left:0;
-            }
-            .categoryName{
-                font-size:15px;
-            }
-        }
-
-        @media screen and (min-width:600px) and (max-width:960px){
-            .category{
-                height:150px;
-                width:150px;
-                float: left;
-                display: inline-block;
-                margin: 40px ;
-            }
-            .categoryName{
-                font-size:15px;
-            }
-        }
-
 
     </style>
 </head>
 <body>
+<?php include 'navBar.php'; ?>
 <div class="page">
     <div class="container1">
         <div class="row d-flex justify-content-around">
@@ -130,7 +61,7 @@ $categories=myFetchAll('select * from categories');
 
                 </div>
                 <?php $user=myFetchOne("select * from user where userid='huangrui1019';"); ?>
-                <small class="text-muted"> <?php echo $user['email'] ?></small>
+                <small class="text-muted"> <?php echo $user['email'] ?>  京ICP备18046047号</small>
                 <p class="text-muted">© 2018 <?php echo $user['name'] ?></p>
             </div>
         </div>
@@ -140,9 +71,9 @@ $categories=myFetchAll('select * from categories');
 </div>
 
 
-<script src="static/assets/vendors/jquery/jquery.js"></script>
-<script src="static/assets/vendors/bootstrap/bootstrap.bundle.min.js"></script>
-<script src="static/assets/vendors/wow/wow.min.js"></script>
+<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdn.bootcss.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="https://cdn.bootcss.com/wow/1.1.2/wow.min.js"></script>
 <script src="static/assets/js/navBar.js"></script>
 
 </body>
